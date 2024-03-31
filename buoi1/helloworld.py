b77ffcc4314a4e96665b5d2dc52b809f9a99aec6
vuminhhieu <vmhieu8.1@gmail.com>
2024-03-31 11:51:48 +0700

    first commit

---
+++ buoi1/helloworld.py
@@ -0,0 +1,51 @@
+# print("Hello, World!")
+# if 5 > 2:
+#    print("Five is greater than two!")
+
+from turtle import *
+
+pen
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# mainloop()
+
+# left(70)
+# forward(100)
+# left(70)
+# forward(80)
+# left(90)
+# forward(80)
+# left(70)
+# forward(100)
+# left(90)
+# forward(50)
+
+
+# forward(100)
+# left(72)
+# forward(100)
+# left(72)
+# forward(100)
+# left(72)
+# forward(100)
+# left(72)
+# forward(100)
+# mainloop()
+
+left(144)
+forward(200)
+right(144)
+forward(200)
+right(144)
+forward(200)
+right(144)
+forward(200)
+right(144)
+forward(200)
+mainloop()
+
